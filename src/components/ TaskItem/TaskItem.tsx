74b3e0e5d39bea463118d80a9aba5755fc93681c
Anton Efimchenko <efimchenkoa@MacBook-Pro-Anton.local>
2024-11-15 19:57:44 +0100

    Add Edit and Delete buttons to TaskItem

--- src/components/ TaskItem/TaskItem.tsx
+++ src/components/ TaskItem/TaskItem.tsx
@@ -1,9 +1,17 @@
 import type { Task } from "../../types";
 import "./TaskItem.css";
+import { Button } from "../Button/Button";
 
 type Props = {
   task: Task;
   onComplete: () => void;
+};
+
+const handleEditTask = () => {
+  console.log("Edited!");
+};
+const handleDeleteTask = () => {
+  console.log("Deleted!");
 };
 
 const TaskItem = ({ task, onComplete }: Props) => {
@@ -16,6 +24,12 @@ const TaskItem = ({ task, onComplete }: Props) => {
         onChange={onComplete}
       />
       <p>{task.title}</p>
+      <Button buttonClass="edit-button" onClick={handleEditTask}>
+        Edit
+      </Button>
+      <Button buttonClass="delete-button" onClick={handleDeleteTask}>
+        Delete
+      </Button>
     </div>
   );
 };
